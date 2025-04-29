d9b4357d5d031f6bb5c150a57765535b31c77132
Drew Jensen <drew.jensen@uvu.edu>
2025-04-29 16:35:53 -0600

    v1.0.9

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ import argparse
 from cli import run_cli
 from db import init_db, set_db_path
 
-VERSION = "v1.0.8"
+VERSION = "v1.0.9"
 
 def main():
   parser = argparse.ArgumentParser(description="TrueSkill League CLI")
